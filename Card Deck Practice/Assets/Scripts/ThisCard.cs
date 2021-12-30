08d4378caa2bde8d5c69a3b91fa283c8a0deb19e
ozenImmovable <95392627+ozenImmovable@users.noreply.github.com>
2021-12-30 17:58:15 -0500

    COMMENTS ON PLAYING CARDS
    empty functions added to ThisCard with comments in them

--- Card Deck Practice/Assets/Scripts/ThisCard.cs
+++ Card Deck Practice/Assets/Scripts/ThisCard.cs
@@ -147,14 +147,43 @@ public class ThisCard : MonoBehaviour
 
     }
     
+    //this function has not yet been edited but will be used for summonign allies                                                                                                     !!!!!!!!!!!!!!!!!
     public void Summon()
     {
+
+        /* This function needs to check if the card is parented to the hand or is currently being dragged or the dropzone it is being dropped on has a sibling
+         * if the card is not parent to the hand, is not being dragged, and has no siblings the ally can be summoned on the dropzone
+         * If it is determined that the card can be summoned on the dropzone the card needs to be destroyed  after it is dropped and a new object needs to be spawned as a child of the dropzone
+         * effects will need to be resolved based on if the card has any
+         * APPLIES TO ALL TYPES: mana value for current mana will need to be decrimented
+         * a seperate function may need to be called to determine which sprite is being spawned
+         */
+
+        //below is what this function originally did/does
         TurnSystem.currentMana -= cost;
         summoned = true;
 
         MaxMana(addXmaxMana);
         drawX = drawXcards;
 
+    }
+
+    public void Cast()
+    {
+        /* this function needs to check if it is hovering over a dropzone
+         * It will also need to determine if the dropzone is an ally dropzone or an enemy based on which card it is
+         * There is the possibility that a spell will target all characters, all allies, or all enemies, this needs to be accounted for usign the cards iD(?) or possibly a seperate function altogether
+         * The basic function of the spell will probably need to be handled in sperate functions, IE drawing cards, dealing damage, etc..... 
+         */
+    }
+
+    public void Modify()
+    {
+        /* This function uses many of the same actions as summoning
+         * The difference to summoning is that this card does not care about the dropzone having children
+         * this function will have to fire another function that determines what it does when played on a dropzone
+         * temporarily this card will destroy itself and then a sprite will be added to that dropzone as a child and sibling to anything currently occupying it
+         */
     }
 
     public void MaxMana(int x)
